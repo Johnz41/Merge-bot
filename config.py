9eab69df704d8ddfa8a70fea7cfeac320b39f82a
Johnz41 <84710033+Johnz41@users.noreply.github.com>
2025-07-19 19:34:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import re
 import os
+from os import environ
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(environ.get("API_ID"))
+API_HASH = environ.get("API_HASH")
+BOT_TOKEN = environ.get("BOT_TOKEN")
